7f708d92320cb6596d69d7a4f41628ae540320c2
rollinginsanity <random@rollinginsanity.net>
2019-05-21 20:42:31 +1000

    Now builds a list of files that we can look at.

---
+++ run.py
@@ -0,0 +1,5 @@
+from scrounge.files import FilesList
+
+files_list = FilesList("sample_files")
+
+files_list.show_file_list()
---
+++ scrounge/files.py
@@ -0,0 +1,27 @@
+import os
+from binaryornot.check import is_binary
+
+class FilesList:
+
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self.file_list = []
+        self.build_file_list()
+
+    def build_file_list(self):
+        """
+        Gets all files within the hierarchy of the base_path.
+        Automatically filters out files that we don't think are binary.
+        :return:
+        """
+        for base, subdirs, files in os.walk(self.base_path):
+
+            for file in files:
+                # We want this to be false.
+                file_path = base + "/" + file
+                if not is_binary(file_path):
+                    self.file_list.append(file_path)
+
+    def show_file_list(self):
+        for file in self.file_list:
+            print(file)

586b65683caab12eae63dc6e47a3208a842ce078
rollinginsanity <random@rollinginsanity.net>
2019-07-09 21:12:34 +1000

    Rolled in binary checks, no external dependencies

--- scrounge/files.py
+++ scrounge/files.py
@@ -1,12 +1,32 @@
 import os
-from binaryornot.check import is_binary
 
 class FilesList:
-
     def __init__(self, base_path):
         self.base_path = base_path
         self.file_list = []
         self.build_file_list()
+
+    def is_binary(self, path):
+        with open(path, 'rb') as f:
+            startbytes = f.read(1024)
+            if not startbytes:
+                print("Empty File")
+                quit()
+
+            if b'\x00' in startbytes:
+                return True
+
+            control_chars = b'\n\r\t\f\b'
+
+            printable_ascii = control_chars + bytes(range(32, 127))
+            non_human_readable = startbytes.translate(None, printable_ascii)
+
+            ratio = len(non_human_readable) / len(startbytes)
+
+            if ratio < 0.3:
+                return False
+            else:
+                return True
 
     def build_file_list(self):
         """
@@ -19,6 +39,7 @@ class FilesList:
             for file in files:
                 # We want this to be false.
                 file_path = base + "/" + file
-                if not is_binary(file_path):
+                if not self.is_binary(file_path):
                     self.file_list.append(file_path)
+
 
